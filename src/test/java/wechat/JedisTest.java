0f767b4a63c12ee0d07dd8ee8d37bae292f3d81c
sophia050506 <sophia050506@sina.com>
2017-05-31 15:15:26 +0800

    jedis
    redis测试案例

---
+++ src/test/java/wechat/JedisTest.java
@@ -0,0 +1,13 @@
+package wechat;
+
+import redis.clients.jedis.Jedis;
+
+public class JedisTest {
+
+	public static void main(String[] args) {
+		Jedis jedis = new Jedis("192.168.88.128",6379);
+		jedis.auth("admin");
+		jedis.set("age", "20");
+		System.out.println(jedis.get("name"));
+	}
+}
